84e33672d7e5433c59fbab71fcb0e8c572b11743
Abdullah Al Noman <opucse8@gmail.com>
2023-12-09 19:09:22 +0100

    feat(thymeleaf): added data to spring mvc model

--- 06 New - Spring MVC/01-thymeleafdemo-helloworld/src/main/java/com/abdullah/thymeleafdemo/controller/HelloWorldController.java
+++ 06 New - Spring MVC/01-thymeleafdemo-helloworld/src/main/java/com/abdullah/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.abdullah.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,14 +10,30 @@ public class HelloWorldController {
     // nedd a controller method to show initial HTML form
 
     @RequestMapping("/showForm")
-    public String showForm(){
+    public String showForm() {
         return "helloworld-form";
     }
 
     // need controller method to process the HTML form
 
     @RequestMapping("/processForm")
-    public String processForm(){
+    public String processForm() {
+        return "helloworld";
+    }
+
+    // need a controller method to read form data
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShouteDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from  the html form
+        String theName = request.getParameter("studentName");
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+        // create the message
+        String result = "Yo ! " + theName;
+        // add message to the model
+        model.addAttribute("message", result);
         return "helloworld";
     }
 }
