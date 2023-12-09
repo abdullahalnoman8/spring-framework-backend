cdd1a1ce88b9e2103dbbd86bb18cb47932414c44
Abdullah Al Noman <opucse8@gmail.com>
2023-12-09 02:33:36 +0100

    Spring security basic configuration

---
+++ 05-spring-security-rest-api/01-spring-boot-rest-security-conf-employee/src/main/java/com/abdullah/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.abdullah.springboot.cruddemo.dao;
+
+import com.abdullah.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
