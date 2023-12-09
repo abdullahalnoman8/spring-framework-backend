7056adaa307baa03e367455fdbe781cfae194f5a
Abdullah Al Noman <opucse8@gmail.com>
2023-12-09 04:16:20 +0100

    feat(spring-security): spring security configuration to use jdbc

--- 05-spring-security-rest-api/01-spring-boot-rest-security-conf-employee/src/main/java/com/abdullah/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-security-rest-api/01-spring-boot-rest-security-conf-employee/src/main/java/com/abdullah/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,41 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+
     private String manager = "MANAGER";
     private String employee = "EMPLOYEE";
     private String admin = "ADMIN";
 
-
+    // add suport for JDBC ... no more hard coded users
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        String password = "{noop}test123";
-        UserDetails john = User.builder()
-                .username("john")
-                .password(password)
-                .roles(employee)
-                .build();
-
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password(password)
-                .roles(employee, manager)
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password(password)
-                .roles(employee, manager, admin)
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
