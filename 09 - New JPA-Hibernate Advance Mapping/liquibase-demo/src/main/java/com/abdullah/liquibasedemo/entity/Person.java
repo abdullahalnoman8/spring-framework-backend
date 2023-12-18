5cc60f13a6b0e607a5393a66d41f55152382990f
Abdullah Al Noman <opucse8@gmail.com>
2023-12-18 01:06:35 +0100

    feat(liquibase): add jpa support table person

---
+++ 09 - New JPA-Hibernate Advance Mapping/liquibase-demo/src/main/java/com/abdullah/liquibasedemo/entity/Person.java
@@ -0,0 +1,16 @@
+package com.abdullah.liquibasedemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+}
